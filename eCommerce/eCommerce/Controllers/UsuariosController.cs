b137a96d3cde95fca74256eee459ae8588893d9a
BrunoTomm <bruno.alexandre.tomm@gmail.com>
2022-10-20 08:55:25 -0300

    MigracaoLocalParaGitHub

---
+++ eCommerce/eCommerce/Controllers/UsuariosController.cs
@@ -0,0 +1,57 @@
+﻿using eCommerceAPI.Models;
+using eCommerceAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private IUsuarioRepository _repository;
+        public UsuariosController()
+        {
+            _repository = new UsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get()); // HTTP 200 - OK
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+
+            if (usuario is null)
+            {
+                return NotFound();// HTTP 400 - Nao encontrado
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Usuario usuario)
+        {
+            _repository.Insert(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Usuario usuario)
+        {
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ eCommerce/eCommerce/Repositories/UsuarioRepository.cs
@@ -0,0 +1,94 @@
+﻿using Dapper;
+using eCommerceAPI.Models;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace eCommerceAPI.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        //Config com banco de dados
+        private IDbConnection _connection;
+        public UsuarioRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public List<Usuario> Get()
+        {
+            return _connection.Query<Usuario>("SELECT * FROM Usuarios").ToList(); //Compara os nomes das tabelas do banco com o objeto, necessario os nomes serem iguais
+        }
+
+        public Usuario Get(int id)
+        {
+            return _connection.Query<Usuario, Contato, Usuario>($" SELECT * FROM Usuarios AS U LEFT JOIN Contatos C ON C.UsuarioId = U.id WHERE U.Id = {id}",
+                (usuario, contato) =>
+                {
+                    usuario.Contato = contato;
+                    return usuario;
+                }
+                ).SingleOrDefault();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            _connection.Open();
+            var transaction = _connection.BeginTransaction(); //Transaction
+
+            try
+            {
+                string sql = "INSERT INTO Usuarios(Nome, Email, Sexo, RG, CPF, NomeMae, SituacaoCadastro, DataCadastro) " +
+                                "VALUES (@Nome, @Email, @Sexo, @RG, @CPF, @NomeMae, @SituacaoCadastro, @DataCadastro); " +
+                                     "SELECT CAST (SCOPE_IDENTITY() AS INT);";
+
+                usuario.Id = _connection.Query<int>(sql, usuario, transaction).Single();
+
+                if (usuario.Contato != null)
+                {
+                    usuario.Contato.UsuarioId = usuario.Id;
+
+                    string sqlContato = "INSERT INTO Contatos(UsuarioId, Telefone, Celular) " +
+                                            "VALUES (@UsuarioId, @Telefone, @Celular); " +
+                                                "SELECT CAST (SCOPE_IDENTITY() AS INT);";
+
+                    usuario.Contato.Id = _connection.Query<int>(sqlContato, usuario.Contato, transaction).Single();
+                }
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    transaction.Rollback();
+                }
+                catch (Exception)
+                {
+                    //Retornar para o UsuarioController alguma mensagem. Ou lancar uma excessao adequada
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+        }
+
+        public void Update(Usuario usuario)
+        {
+            string sql = "UPDATE Usuarios SET Nome = @Nome, Email  = @Email, Sexo = @Sexo, RG = @RG, CPF = @CPF, NomeMae = @NomeMae, SituacaoCadastro = @SituacaoCadastro, DataCadastro = @DataCadastro " +
+                "WHERE Id = @Id";
+
+            _connection.Execute(sql, usuario);
+
+        }
+        public void Delete(int id)
+        {
+            _connection.Execute("DELETE FROM Usuarios WHERE Id = @Id", new { Id = id });
+        }
+    }
+}
