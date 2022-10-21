1c310816fbc4d1fe45319f455d6ff0b1ef012006
BrunoTomm <bruno.alexandre.tomm@gmail.com>
2022-10-21 11:24:21 -0300

    AlteradoMetodoGet
    Alterado para obter, Usuario, Contato e Endereço de entrega.

--- eCommerce/eCommerce/Repositories/UsuarioRepository.cs
+++ eCommerce/eCommerce/Repositories/UsuarioRepository.cs
@@ -20,9 +20,35 @@ namespace eCommerceAPI.Repositories
 
         public List<Usuario> Get()
         {
-            return _connection.Query<Usuario>("SELECT * FROM Usuarios").ToList(); //Compara os nomes das tabelas do banco com o objeto, necessario os nomes serem iguais
-        }
+            //return _connection.Query<Usuario>("SELECT * FROM Usuarios").ToList(); //Compara os nomes das tabelas do banco com o objeto, necessario os nomes serem iguais
+            List<Usuario> usuariosList = new List<Usuario>();
 
+            string sql = $@"SELECT * FROM Usuarios AS U 
+	                            LEFT JOIN Contatos C ON C.UsuarioId = U.id 
+	                            LEFT JOIN EnderecosEntrega EE ON EE.UsuarioId = U.Id";
+
+            _connection.Query<Usuario, Contato, EnderecoEntrega, Usuario>(sql,
+                    (usuario, contato, enderecoEntrega) =>
+                    {
+                        if(usuariosList.SingleOrDefault(_ => _.Id == usuario.Id) == null)
+                        {
+                            usuario.EnderecosEntrega = new List<EnderecoEntrega>();
+                            usuario.Contato = contato;
+                            usuariosList.Add(usuario);
+                        }
+                        else
+                        {
+                            usuario = usuariosList.SingleOrDefault(_ => _.Id == usuario.Id);
+                        }
+
+                        usuario.EnderecosEntrega.Add(enderecoEntrega);
+
+                        return usuario;
+                    });
+
+            return usuariosList;
+        }
+          
         public Usuario Get(int id)
         {
             return _connection.Query<Usuario, Contato, Usuario>($" SELECT * FROM Usuarios AS U LEFT JOIN Contatos C ON C.UsuarioId = U.id WHERE U.Id = {id}",
@@ -80,14 +106,40 @@ namespace eCommerceAPI.Repositories
 
         public void Update(Usuario usuario)
         {
-            string sql = "UPDATE Usuarios SET Nome = @Nome, Email  = @Email, Sexo = @Sexo, RG = @RG, CPF = @CPF, NomeMae = @NomeMae, SituacaoCadastro = @SituacaoCadastro, DataCadastro = @DataCadastro " +
-                "WHERE Id = @Id";
+            _connection.Open();
+            var transaction = _connection.BeginTransaction();
 
-            _connection.Execute(sql, usuario);
+            try
+            {
+                string sql = "UPDATE Usuarios SET Nome = @Nome, Email  = @Email, Sexo = @Sexo, RG = @RG, CPF = @CPF, NomeMae = @NomeMae, SituacaoCadastro = @SituacaoCadastro, DataCadastro = @DataCadastro WHERE Id = @Id";
+                _connection.Execute(sql, usuario, transaction);
+
+                if (usuario.Contato != null)
+                {
+                    string sqlContato = "UPDATE Contatos SET Telefone = @Telefone, Celular = @Celular WHERE Id = @Id";
+                    _connection.Execute(sqlContato, usuario.Contato, transaction);
+                }
+
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    transaction.Rollback();
+                }
+                catch (Exception)
+                {}
+            }
+            finally
+            {
+                _connection.Close();
+            }
 
         }
         public void Delete(int id)
         {
+            //Já é configurado no banco ON DELETE CASCADE, dizendo que todos vinculados ao UsuarioId deletado serão excluídos em um efeito cascata
             _connection.Execute("DELETE FROM Usuarios WHERE Id = @Id", new { Id = id });
         }
     }
