bdbb13d5370a5836fbe1e4337bb361313bdc3d60
BrunoTomm <bruno.alexandre.tomm@gmail.com>
2022-12-14 15:09:30 -0300

    Transferencia De Projeto

---
+++ eCommerce/eCommerce/Controllers/ContribUsuariosController.cs
@@ -0,0 +1,58 @@
+﻿using eCommerce.Repositories;
+using eCommerceAPI.Models;
+using eCommerceAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    [Route("api/Contrib/[controller]")]
+    [ApiController]
+    public class ContribUsuariosController : ControllerBase
+    {
+        private IUsuarioRepository _repository;
+        public ContribUsuariosController()
+        {
+            _repository = new ContribUsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get()); // HTTP 200 - OK
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+
+            if (usuario is null)
+            {
+                return NotFound();// HTTP 400 - Nao encontrado
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Usuario usuario)
+        {
+            _repository.Insert(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Usuario usuario)
+        {
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ eCommerce/eCommerce/Repositories/ContribUsuarioRepository.cs
@@ -0,0 +1,34 @@
+﻿using eCommerceAPI.Models;
+using eCommerceAPI.Repositories;
+using System.Collections.Generic;
+
+namespace eCommerce.Repositories
+{
+    public class ContribUsuarioRepository : IUsuarioRepository
+    {
+
+        public List<Usuario> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Usuario Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
